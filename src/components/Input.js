87a3b4c651a4e4308ca45f2818243866d2c3dc10
codeconcept <contact@codeconcept.fr>
2019-11-20 11:18:07 +0100

    Handle change on the input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,30 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Input = ({ isLast }) => {
   const lastInput = useRef(null);
+  const [text, setText] = useState("");
+
   useEffect(() => {
     console.log("lastInput", lastInput);
     lastInput.current.focus();
   }, []);
 
+  const handleChange = e => {
+    setText(e.target.value);
+  };
+
   return (
     <>
-      {isLast ? <input type="text" ref={lastInput} /> : <input type="text" />}
+      {isLast ? (
+        <input
+          type="text"
+          ref={lastInput}
+          value={text}
+          onChange={handleChange}
+        />
+      ) : (
+        <input type="text" />
+      )}
       <br />
     </>
   );
