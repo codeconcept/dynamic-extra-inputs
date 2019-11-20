b16292668996de24e799b250fb31eb63fd0e516a
codeconcept <contact@codeconcept.fr>
2019-11-20 12:30:23 +0100

    Save new data on blur

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Input from "./components/Input";
 
 function App() {
   const [nbInputs, setNbInputs] = useState([Date.now()]);
+  const [allInputsValues, setAllInputsValues] = useState([]);
 
   const addInput = () => {
     setNbInputs(nbInputs.concat([Date.now()]));
@@ -13,10 +14,39 @@ function App() {
     return nbInputs.length === ++index;
   };
 
+  const handleBlur = data => {
+    let newValues = [];
+    if (allInputsValues.length === 0) {
+      newValues = [data];
+    } else {
+      const allreadyInArray = allInputsValues.find(obj => obj.id === data.id);
+      if (!allreadyInArray) {
+        // add
+        newValues = [...allInputsValues, data];
+      } else {
+        // update
+        newValues = allInputsValues.map(inputValue =>
+          inputValue.id === data.id ? data : inputValue
+        );
+      }
+    }
+    setAllInputsValues(newValues);
+  };
+
   return (
     <div className="App">
+      <div>
+        <code>
+          <pre>{JSON.stringify(allInputsValues, null, 4)}</pre>
+        </code>
+      </div>
       {nbInputs.map((nb, index) => (
-        <Input key={nb} isLast={isLast(index)} />
+        <Input
+          key={nb}
+          id={nb}
+          isLast={isLast(index)}
+          handleBlur={handleBlur}
+        />
       ))}
       <br />
       <button onClick={addInput}>+</button>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 
-const Input = ({ isLast }) => {
+const Input = ({ isLast, id, handleBlur }) => {
   const lastInput = useRef(null);
   const [text, setText] = useState("");
 
@@ -18,12 +18,19 @@ const Input = ({ isLast }) => {
       {isLast ? (
         <input
           type="text"
+          id={id}
           ref={lastInput}
           value={text}
           onChange={handleChange}
+          onBlur={() => handleBlur({ id, value: text })}
         />
       ) : (
-        <input type="text" />
+        <input
+          type="text"
+          value={text}
+          onChange={handleChange}
+          onBlur={() => handleBlur({ id, value: text })}
+        />
       )}
       <br />
     </>
